da4da16418daae50ff39250cb0a18774e11414df
Nick Papadakis <nickfin2014@gmail.com>
2022-12-01 16:55:21 -0700

    okay, basic layout and ui library for interacting with API. lets do what we are supposed to primarily now

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,15 +1,19 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { TopNav } from './components/TopNav';
+import { Footer } from './components/Footer';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to client!</title>
+        <title>STS Demo!</title>
       </Head>
-      <main className="app">
+      <main className="app flex flex-col h-screen justify-between">
+        <TopNav />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
   );
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,10 +1,10 @@
-import { createGlobPatternsForDependencies } from '@nrwl/react/tailwind';
-import { join } from 'path';
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
 
 module.exports = {
   content: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
-    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
