ed26382bb3d98862a938602f6e4d1f5cc75a10f9
Nick Papadakis <nickfin2014@gmail.com>
2022-12-01 16:11:21 -0700

    added tailwind as styling for rudimentary client

---
+++ apps/client/tailwind.config.ts
@@ -0,0 +1,14 @@
+import { createGlobPatternsForDependencies } from '@nrwl/react/tailwind';
+import { join } from 'path';
+
+module.exports = {
+  content: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
