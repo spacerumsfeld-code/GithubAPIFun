c500efd909620c8bda25527aeb32102b4b10078d
Nick Papadakis <nickfin2014@gmail.com>
2022-12-01 18:17:13 -0700

    authed with github, surprisingly annoying

--- libs/services/github/src/lib/github.service.ts
+++ libs/services/github/src/lib/github.service.ts
@@ -1,17 +1,26 @@
-import octokitClient from './octokit.client';
+import { createTokenAuth } from '@octokit/auth-token';
+import { Octokit } from 'octokit';
 
 export class GithubService {
-  private readonly client = octokitClient;
-
   async getFollowersById(username: string) {
+    const auth = createTokenAuth('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');
+    const { token } = await auth();
+
+    console.log('what token are we getting', token);
+    const client = new Octokit({
+      auth: token,
+    });
+
+    console.log('client?', client);
     let followers;
 
     try {
-      followers = await this.client.request(`GET /users/${username}/followers`);
+      followers = await client.request(`GET /users/${username}/followers`);
     } catch (e) {
       console.log(e);
     }
-    console.log('username!', username);
+
+    console.log('followers from github method', followers);
 
     return followers;
   }
--- libs/services/github/src/lib/octokit.client.ts
+++ libs/services/github/src/lib/octokit.client.ts
@@ -1,4 +1,5 @@
 import { Octokit } from 'octokit';
+import { createTokenAuth } from '@octokit/auth-token';
 
 const octokitClient = new Octokit({
   auth: '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',
